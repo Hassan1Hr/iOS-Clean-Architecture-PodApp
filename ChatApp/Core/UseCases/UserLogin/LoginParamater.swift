b582d27558dc88903c319fd773acb2615f7c8797
Hassan Ali <hassanramadan4443@gmail.com>
2022-11-29 16:05:41 +0200

    Add UserLoginUseCase with it's implementation

---
+++ ChatApp/Core/UseCases/UserLogin/LoginParamater.swift
@@ -0,0 +1,13 @@
+//
+//  LoginParamater.swift
+//  ChatApp
+//
+//  Created by mac on 29/11/2022.
+//
+
+import Foundation
+
+struct LoginParamater{
+    var name: String
+    var email: String
+}
---
+++ ChatApp/Core/UseCases/UserLogin/UserLogin.swift
@@ -0,0 +1,23 @@
+//
+//  UserLogin.swift
+//  ChatApp
+//
+//  Created by mac on 29/11/2022.
+//
+
+import Foundation
+
+protocol UserLoginUseCase{
+    func userLogin(
+        loginParamater: LoginParamater,
+        completionHandler: @escaping (_ user: Result<User,Error>)-> Void
+    )
+}
+
+class UserLoginUseCaseImpl: UserLoginUseCase{
+    func userLogin(loginParamater: LoginParamater, completionHandler: @escaping (Result<User, Error>) -> Void) {
+        
+    }
+    
+    
+}
