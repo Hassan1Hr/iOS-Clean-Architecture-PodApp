62b7df8853b67b25a49908ceca49e491c59abf3c
Hassan Ali <hassanramadan4443@gmail.com>
2022-11-29 13:58:11 +0200

    Add User Entity

---
+++ ChatApp/Core/Entities/User.swift
@@ -0,0 +1,17 @@
+//
+//  User.swift
+//  ChatApp
+//
+//  Created by mac on 29/11/2022.
+//
+
+import Foundation
+
+struct User:Codable {
+    var id = ""
+    var username:String
+    var email:String
+    var pushId = ""
+    var avatarLink = ""
+    var status:String
+}
