9e3ed28a1088ffc99e4ebfc2531ad98470ded429
Hassan Ali <hassanramadan4443@gmail.com>
2022-11-30 10:49:27 +0200

    Add implementation for UserLogin usecase

--- ChatApp/Core/UseCases/UserLogin/UserLogin.swift
+++ ChatApp/Core/UseCases/UserLogin/UserLogin.swift
@@ -7,19 +7,32 @@
 
 import Foundation
 
-protocol UserLoginUseCase{
+protocol UserLoginUseCase {
     func userLogin(
         loginParamater: LoginParamater,
-        completionHandler: @escaping (_ user: Result<User,Error>)-> Void
+        completionHandler: @escaping (_ user: Result<User, Error>) -> Void
     )
 }
 
-class UserLoginUseCaseImpl: UserLoginUseCase{
+class UserLoginUseCaseImpl: UserLoginUseCase {
+    let authentecationGateway: AuthentecationGateway
+    
+    init(authentecationGateway: AuthentecationGateway) {
+        self.authentecationGateway = authentecationGateway
+    }
     func userLogin(
         loginParamater: LoginParamater,
         completionHandler: @escaping (Result<User, Error>) -> Void) {
-        
+            guard loginParamater.password.count > 8 else {
+                completionHandler(.failure(UserLoginError.passwordNotLongEnough))
+                return
+            }
+            guard loginParamater.email.isValidEmail() else {
+                completionHandler(.failure(UserLoginError.notValidEmail))
+                return
+            }
+            authentecationGateway.login(loginParamter: loginParamater,
+                                        completionHandler: completionHandler)
     }
-    
     
 }
