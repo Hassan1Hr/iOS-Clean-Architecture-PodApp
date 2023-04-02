f1c0be63e819a41b50900ed2d1f9d1cb624dad1c
Hassan Ali <hassanramadan4443@gmail.com>
2023-04-02 09:41:34 +0200

    Define base  request & it's Type

---
+++ MyPodcasts/EntityGateway/API/Client/Networking/Request/RequestType.swift
@@ -0,0 +1,13 @@
+//
+//  RequestType.swift
+//  MyPodcasts
+//
+//  Created by mac on 02/04/2023.
+//
+
+import Foundation
+
+enum RequestType: String {
+  case GET
+  case POST
+}
---
+++ MyPodcasts/EntityGateway/API/Client/Networking/Request/podcast/PodcastRequest.swift
@@ -0,0 +1,8 @@
+//
+//  PodcastRequest.swift
+//  MyPodcasts
+//
+//  Created by mac on 02/04/2023.
+//
+
+import Foundation
