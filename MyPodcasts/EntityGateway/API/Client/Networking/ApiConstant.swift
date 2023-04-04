6678d4145d8af3f7023ded93f0db15449523344d
Hassan Ali <hassanramadan4443@gmail.com>
2023-04-05 00:49:02 +0200

    Add podcastRequest to builde a request of podact

--- MyPodcasts/EntityGateway/API/Client/Networking/ApiConstant.swift
+++ MyPodcasts/EntityGateway/API/Client/Networking/ApiConstant.swift
@@ -8,6 +8,6 @@
 import Foundation
 
 enum APIConstants {
-  static let host = "api.petfinder.com"
-  static let apiKey = "YourKeyHere"
+  static let host = "listen-api.listennotes.com/api/v2/"
+  static let apiKey = "824b0a4c3f7849de9a60484e16e85424"
 }
--- MyPodcasts/EntityGateway/API/Client/Networking/Request/podcast/PodcastRequest.swift
+++ MyPodcasts/EntityGateway/API/Client/Networking/Request/podcast/PodcastRequest.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+
+enum PodcastRequest: RequestProtocol {
+    case getPopularPodcastsWith(page: Int)
+    case fetchRandomPodcast
+    case getCategoryWith(genreId: Int, page: Int, region: String)
+    
+    var path: String {
+        switch self{
+        case .getPopularPodcastsWith:
+            return "curated_podcasts"
+        case .fetchRandomPodcast:
+            return "just_listen"
+        case .getCategoryWith:
+            return "best_podcasts"
+        }
+    }
+    
+    var urlParams: [String: String?] {
+        switch self {
+        case let .getPopularPodcastsWith(page):
+            return ["page": String(page)]
+            
+        case  .fetchRandomPodcast:
+            return [:]
+            
+        case let .getCategoryWith(genreId, page, _):
+            return ["page": String(page), "genre_id": String(genreId)]
+        }
+    }
+    
+    var requestType: RequestType {
+        .GET
+    }
+}
