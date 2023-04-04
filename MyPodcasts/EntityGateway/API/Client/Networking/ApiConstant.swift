42043283c74c0126b323967bd42d589537d877c1
Hassan Ali <hassanramadan4443@gmail.com>
2023-04-05 00:18:01 +0200

    Add APIConstants

---
+++ MyPodcasts/EntityGateway/API/Client/Networking/ApiConstant.swift
@@ -0,0 +1,13 @@
+//
+//  ApiConstant.swift
+//  MyPodcasts
+//
+//  Created by mac on 05/04/2023.
+//
+
+import Foundation
+
+enum APIConstants {
+  static let host = "api.petfinder.com"
+  static let apiKey = "YourKeyHere"
+}
