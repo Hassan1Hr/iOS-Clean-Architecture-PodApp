b294bebda43a2bf4cf0ac3d367f01488b6d46747
Hassan Ramadan <hassanramadan4443@gmail.com>
2023-04-11 08:22:34 +0200

    feat: Edit Remote Network (#4)
    * rename $ organize some folder
    
    * Define base  request & it's Type
    
    * Add NetworkError class
    
    * Add APIConstants
    
    * Add general DataParser to parse generic data come from api
    
    * Add podcastRequest to builde a request of podact
    
    * Enhance Network layer to depend on async-await rather than Alamofire
    
    * Remove some network dependency

---
+++ MyPodcasts/EntityGateway/API/Client/Networking/ApiConstant.swift
@@ -0,0 +1,13 @@
+//
+//  ApiConstant.swift
+//  MyPodcasts
+//
+//  Created by mac on 05/04/2023.
+//
+
+import Foundation
+
+enum APIConstants {
+  static let host = "listen-api.listennotes.com/api/v2/"
+  static let apiKey = "824b0a4c3f7849de9a60484e16e85424"
+}
