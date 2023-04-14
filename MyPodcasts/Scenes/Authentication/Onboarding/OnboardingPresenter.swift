8d7bb757f68e30891aa3ab6d01507cb183a9c08c
Hassan Ramadan <hassanramadan4443@gmail.com>
2023-04-14 11:19:30 +0200

    Feat: Create Onboarding Module (#5)
    * rename $ organize some folder
    
    * Define base  request & it's Type
    
    * Add NetworkError class
    
    * Add APIConstants
    
    * Add general DataParser to parse generic data come from api
    
    * Add podcastRequest to builde a request of podact
    
    * Enhance Network layer to depend on async-await rather than Alamofire
    
    * Remove some network dependency
    
    * Add onboarding images assets files
    
    * Add onboarding presentation layer
    
    -OnboardingPresenter 
    -OnboardingView
    -OnboardingViewCell
    -OnboardingViewController
    
    * Define Onboarding navigation layer 
    
    - implementing Routes protocol
    - make onboardingPresenter define it's destination
    
    * update Alamofire & Kingfisher pods

--- MyPodcasts/Scenes/Authentication/Onboarding/OnboardingPresenter.swift
+++ MyPodcasts/Scenes/Authentication/Onboarding/OnboardingPresenter.swift
@@ -8,20 +8,54 @@
 import Foundation
 
 protocol OnboardingPresenter {
+    var  slides: [OnboardingSlide] { get }
+    func nextBtnClicked()
+    func updateCurrentPage(currentPage: Int)
     func openLogin()
     func openSignup()
 }
 
 class OnboardingPresenterImpl: OnboardingPresenter {
     
-    let onboardingView: OnboardingView
+    private weak var onboardingView: OnboardingView?
+    var slides: [OnboardingSlide] = [
+        OnboardingSlide(title: "Delicious Dishes", description: "Experience a variety of amazing dishes from different cultures around the world.", image: #imageLiteral(resourceName: "slide2")),
+        OnboardingSlide(title: "World-Class Chefs", description: "Our dishes are prepared by only the best.", image: #imageLiteral(resourceName: "slide1")),
+        OnboardingSlide(title: "Instant World-Wide Delivery", description: "Your orders will be delivered instantly irrespective of your location around the world.", image: #imageLiteral(resourceName: "slide3"))
+    ]
+    
+    var currentPage = 0 {
+        didSet {
+            onboardingView?.setCurrentPage(currentPage: currentPage)
+            if currentPage == slides.count - 1 {
+                onboardingView?.updateNextBtnTitle(title: "Get Started")
+            } else {
+                onboardingView?.updateNextBtnTitle(title: "Next")
+            }
+        }
+    }
     
     init(view: OnboardingView) {
         self.onboardingView = view
     }
     
+    func updateCurrentPage(currentPage: Int) {
+        self.currentPage = currentPage
+    }
+    
+    func nextBtnClicked() {
+        if currentPage == slides.count - 1 {
+            onboardingView?.navigate(OnboardingRoutes.home)
+        } else {
+            currentPage += 1
+            let indexPath = IndexPath(item: currentPage, section: 0)
+            onboardingView?.scrollToNextPage(indexPath: indexPath)
+           
+        }
+    }
+    
     func openLogin() {
-//        onboardingView?.navigate(OnboardingRoutes.login)
+       
     }
     
     func openSignup() {
@@ -29,3 +63,5 @@ class OnboardingPresenterImpl: OnboardingPresenter {
         return
     }
 }
+
+
--- MyPodcasts/Scenes/Authentication/Onboarding/OnboardingViewController.swift
+++ MyPodcasts/Scenes/Authentication/Onboarding/OnboardingViewController.swift
@@ -7,21 +7,64 @@
 
 import UIKit
 
-protocol OnboardingView {
-    
+protocol OnboardingView: NavigationRoute{
+    func updateNextBtnTitle(title: String)
+    func setCurrentPage(currentPage: Int)
+    func scrollToNextPage(indexPath: IndexPath)
 }
 
 class OnboardingViewController: UIViewController {
     var presenter: OnboardingPresenter!
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var nextBtn: UIButton!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func nextBtnClicked(_ sender: Any) {
+        presenter.nextBtnClicked()
+    }
+    
 }
 
 extension OnboardingViewController: OnboardingView {
+    func scrollToNextPage(indexPath: IndexPath) {
+        collectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+    }
     
+    func setCurrentPage(currentPage: Int) {
+        pageControl.currentPage = currentPage
+    }
+    
+    func updateNextBtnTitle(title: String) {
+        nextBtn.setTitle(title, for: .normal)
+    }
+}
+
+extension OnboardingViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return presenter.slides.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: OnboardingCollectionViewCell.identifier,
+            for: indexPath) as! OnboardingCollectionViewCell
+        cell.setup(presenter.slides[indexPath.row])
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: collectionView.frame.height)
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        let width = scrollView.frame.width
+        presenter.updateCurrentPage(currentPage: Int(scrollView.contentOffset.x / width)) 
+    }
 }
